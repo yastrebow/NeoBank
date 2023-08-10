952d69b9e07817f47a4a5e7e7994d1c6c1f66215
Alexander Peregudov <aperegudov@neoflex.ru>
2023-08-10 17:58:34 +0300

    added response to addAccount + generation last 7 numbers of accountNumber

--- msa-bank-account-cs/pkg/services/service.go
+++ msa-bank-account-cs/pkg/services/service.go
@@ -1,6 +1,9 @@
 package services
 
 import (
+	"crypto/rand"
+	"math"
+	"math/big"
 	"msa-bank-account-cs/models"
 
 	openapi_types "github.com/deepmap/oapi-codegen/pkg/types"
@@ -11,7 +14,7 @@ type accountRepository interface {
 	GetAccounts() (*[]models.Account, error)
 	AddAccount(e *models.Account) (*models.Account, error)
 	UpdateAccount(e *models.Account) (*models.Account, error)
-	DeleteAccount(id  openapi_types.UUID) (*models.Account, error)
+	DeleteAccount(id openapi_types.UUID) (*models.Account, error)
 	UpdateAccountBalance(id openapi_types.UUID, e *models.ChangeAccountBalance) (*models.Account, error)
 }
 
@@ -25,8 +28,8 @@ func AccountService(accountRepo accountRepository) Account {
 	}
 }
 
-	// Get account from the store
-	// (GET /account/{id})
+// Get account from the store
+// (GET /account/{id})
 func (b *Account) GetAccountService(clientId openapi_types.UUID) (*models.Account, error) {
 	account, err := b.accountRepo.GetAccount(clientId)
 	if err != nil {
@@ -36,9 +39,43 @@ func (b *Account) GetAccountService(clientId openapi_types.UUID) (*models.Accoun
 	return account, nil
 }
 
-	// Add a new account to the store
-	// (POST /account)
-func (b *Account) AddAccountService(e *models.Account) (*models.Account, error ){
+func generateRandomNumber(numberOfDigits int) (int, error) {
+	maxLimit := int64(int(math.Pow10(numberOfDigits)) - 1)
+	lowLimit := int(math.Pow10(numberOfDigits - 1))
+
+	randomNumber, err := rand.Int(rand.Reader, big.NewInt(maxLimit))
+	if err != nil {
+		return 0, err
+	}
+	randomNumberInt := int(randomNumber.Int64())
+
+	if randomNumberInt <= lowLimit {
+		randomNumberInt += lowLimit
+	}
+
+	if randomNumberInt > int(maxLimit) {
+		randomNumberInt = int(maxLimit)
+	}
+	return randomNumberInt, nil
+}
+
+// Add a new account to the store
+// (POST /account)
+func (b *Account) AddAccountService(e *models.Account) (*models.Account, error) {
+	if e.AccountNumber == nil {
+		return nil, nil
+	}
+
+	randNum, err := generateRandomNumber(7)
+	if err != nil {
+		return nil, err
+	}
+
+	accNum := *e.AccountNumber
+
+	accNum = accNum + string(rune(randNum))
+
+	*e.AccountNumber = accNum
 	account, err := b.accountRepo.AddAccount(e)
 	if err != nil {
 		return nil, err
@@ -47,9 +84,9 @@ func (b *Account) AddAccountService(e *models.Account) (*models.Account, error )
 	return account, nil
 }
 
-	// Update an existing account
-	// (PUT /account)
-func (b *Account) UpdateAccountService(e *models.Account) (*models.Account, error ){
+// Update an existing account
+// (PUT /account)
+func (b *Account) UpdateAccountService(e *models.Account) (*models.Account, error) {
 	account, err := b.accountRepo.UpdateAccount(e)
 	if err != nil {
 		return nil, err
@@ -58,8 +95,8 @@ func (b *Account) UpdateAccountService(e *models.Account) (*models.Account, erro
 	return account, nil
 }
 
-	// Delete account from the store
-	// (DELETE /account/{id})
+// Delete account from the store
+// (DELETE /account/{id})
 func (b *Account) DeleteAccountService(id openapi_types.UUID) (*models.Account, error) {
 	account, err := b.accountRepo.DeleteAccount(id)
 	if err != nil {
@@ -69,20 +106,20 @@ func (b *Account) DeleteAccountService(id openapi_types.UUID) (*models.Account, 
 	return account, nil
 }
 
-	// Get all accounts from the store
-	// (GET /accounts)
+// Get all accounts from the store
+// (GET /accounts)
 func (b *Account) GetAccountsService() (*[]models.Account, error) {
-		accounts, err := b.accountRepo.GetAccounts()
-		if err != nil {
-			return nil, err
-		}
-	
-		return accounts, nil
+	accounts, err := b.accountRepo.GetAccounts()
+	if err != nil {
+		return nil, err
+	}
+
+	return accounts, nil
 }
 
-	// update balance for account from the store
-	// (POST /account/{id}/update-balance)
-func (b *Account) UpdateAccountBalanceService(id openapi_types.UUID, e *models.ChangeAccountBalance) (*models.Account, error ){
+// update balance for account from the store
+// (POST /account/{id}/update-balance)
+func (b *Account) UpdateAccountBalanceService(id openapi_types.UUID, e *models.ChangeAccountBalance) (*models.Account, error) {
 	account, err := b.accountRepo.UpdateAccountBalance(id, e)
 	if err != nil {
 		return nil, err
